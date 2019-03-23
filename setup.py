02ad82b089acc663218ce2e4e7d79596467ca45e
BrandtWP <bwinklerprins@yahoo.com>
2019-03-23 13:22:20 -0700

    fixes

--- setup.py
+++ setup.py
@@ -3,4 +3,5 @@ import notebook
 
 location = os.path.dirname(os.path.realpath(__file__))
 notebookPath = os.path.dirname(notebook.__file__)
+print(location, notebookPath, 'cp %s/custom.css %s/static/custom/custom.css' % (location, notebookPath))
 os.popen('cp %s/custom.css %s/static/custom/custom.css' % (location, notebookPath))
